c9da93dbba50a325a764fc1c6546b9c150ce558f
conradcorbett <conrad.corbett@hashicorp.com>
2023-02-07 20:35:49 -0800

    added bracket

--- aws/infra/main.tf
+++ aws/infra/main.tf
@@ -65,7 +65,7 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  vpc_security_group_ids = aws_security_group.sg-vpc.id
+  vpc_security_group_ids = [ aws_security_group.sg-vpc.id ]
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
